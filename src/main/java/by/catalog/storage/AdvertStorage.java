28cc8cc65d4235e527d04ec2e555ea73dcdadc08
LukshaMikalai <lucshahomm@gmail.com>
2020-06-07 22:40:27 +0300

    Add method from AddAdvert (save in tables)

--- src/main/java/by/catalog/storage/AdvertStorage.java
+++ src/main/java/by/catalog/storage/AdvertStorage.java
@@ -2,9 +2,32 @@ package by.catalog.storage;
 
 import by.catalog.domain.Advert;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AdvertStorage {
+
+    Connection connection = null;
+    private final static String URL_TABLES = "jdbc:postgresql://localhost:5432/postgres";
+    private final static String LOGIN_TABLES = "postgres";
+    private final static String PASS_TABLES = "1987Roll";
 
     public void addAdvert(Advert advert){
 
+            try {
+                connection = DriverManager.getConnection(URL_TABLES, LOGIN_TABLES, PASS_TABLES);
+                PreparedStatement preparedStatement = connection.prepareStatement("insert into advert (id, model, color, yearcar, price, id_user) values (default , ? , ? , ? , ?, ?)");
+                preparedStatement.setString(1, advert.getModelCar());
+                preparedStatement.setString(2, advert.getColorCar());
+                preparedStatement.setInt(3, advert.getYearCar());
+                preparedStatement.setDouble(4, advert.getPriceCar());
+                preparedStatement.setLong(5, advert.getId_user());
+                preparedStatement.executeQuery();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
-}
